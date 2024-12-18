f7e60b4be1801554ba6e75fd83dbbb2efa84261b
CoffeElliot <142987003+CoffeElliot@users.noreply.github.com>
2024-12-18 20:16:40 +0100

    charge close to done, battery stared

--- slprj/load_vars.m
+++ slprj/load_vars.m
@@ -1,4 +1,10 @@
 %load all the variabels and functions needed for simulation
 
+clear,clc;
+%-------------------- Battery variabels --------------------%
 Is_chargin = 1; %bool if the system charges or not
-Charge_rate = 12*60; %Volt*Amph
+Charge_rate = 10; %10Amph ;
+Charge_loss_rate = 0.2;% amount of amp that get lost during chargin for heat and so on.
+Battery_current_charge = 0;
+Batter_max_charge = 60;%60Amph;
+
