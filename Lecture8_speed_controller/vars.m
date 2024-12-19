9995a5f6297a62777b0d95722ade0f02fca9d123
CoffeElliot <142987003+CoffeElliot@users.noreply.github.com>
2024-12-19 12:52:10 +0100

    lecture 8

---
+++ Lecture8_speed_controller/vars.m
@@ -0,0 +1,25 @@
+%lecture 8
+
+% We have a speed camera that desides to open a beam or let it be closed
+% depending on a car speed.
+
+% For the beam:simscape->fundation->mechanical->rotation
+% dubbel click the motor-> select part to select motor type
+
+%logg sim signal->3punkter->"wifi" symbol
+    % simulaton -> data inspecor
+
+%Simulink have a PID controller, and you can auto tune it within the PID
+%controller, by pressing the "tune" button längst ner
+    % block response = current controller
+    % tuned response = Matlab recommended controller
+
+%Trapezoidal Velocity Profile Trajectory -> smooth transition between 2
+%values within a time period
+    %if code block is slow dubble click code block -> simulation using -> Code generation
+
+
+% state flow -> chart = transition between states
+    % redomended onramp stateflow
+
+%did not have time to implement close command
