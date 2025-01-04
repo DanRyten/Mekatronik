d8bf456c0fd039fc87ff9e90c7afcd0df6081104
CoffeElliot <142987003+CoffeElliot@users.noreply.github.com>
2025-01-04 15:19:33 +0100

    vars update

--- slprj/load_vars.m
+++ slprj/load_vars.m
@@ -1,3 +1,8 @@
 % vars
 
-Weight = 380 + 72.2; % tuktuk and driver
+current_motor_watt = 331.8;
+real_motor_watt = 3000;
+
+scaler = current_motor_watt/real_motor_watt;
+
+Weight = scaler * (380 + 72.2); % tuktuk and driver
