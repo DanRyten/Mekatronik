5a28a0a42f26f29da48cf0902071a83e87cce5fc
CoffeElliot <142987003+CoffeElliot@users.noreply.github.com>
2025-01-05 17:41:09 +0100

    added basic translation function

--- slprj/load_vars.m
+++ slprj/load_vars.m
@@ -1,8 +1,17 @@
 % vars
-
+clear,clc;
+% battery
 current_motor_watt = 331.8;
 real_motor_watt = 3000;
+battery_volt = 60;
 
+%const
 scaler = current_motor_watt/real_motor_watt;
+gravitation = 9.82;
 
+%tuk tuk const
 Weight = scaler * (380 + 72.2); % tuktuk and driver
+
+%translation const
+Gear_box_ratio = 5;
+Wheel_radius = scaler*0.2;
