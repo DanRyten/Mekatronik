9473765e5d1d8f4c6db69923e50d01bec94ee694
CoffeElliot <142987003+CoffeElliot@users.noreply.github.com>
2025-01-07 13:59:07 +0100

    more tweeks

--- slprj/load_vars.m
+++ slprj/load_vars.m
@@ -10,16 +10,16 @@ scaler =  current_motor_watt/real_motor_watt;
 
 % battery
 battery_voltage = 60; %volt
-%max current -> 331.8/60 = 5.63amp
+%max current -> 331.8/60 = 5.63amp -> 
 battery_volt = battery_voltage;
-current_limiting_resistor = scaler*battery_voltage/(real_motor_watt/battery_voltage); %V/Amp = ohm
+current_limiting_resistor = (battery_voltage/(real_motor_watt/battery_voltage));%*scaler; %V/Amp = ohm
 
 %tuk tuk const
 Weight = scaler * (380 + 72.2); % tuktuk and driver
 
 %translation const
 Gear_box_ratio = 5;
-Wheel_radius = scaler*0.2;
+Wheel_radius = 0.2;%*scaler;
 
 slope = -10; %degrees ie 90 = max
 desired_speed = 1;
